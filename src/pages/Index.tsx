8f115604e1bbfe0bfa733566a9821ebe46fd1f04
nicholasgloria <nicholassunderhus@gmail.com>
2025-11-29 23:47:32 -0300

    voltei atras dnv

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import ActiveRaffles from "@/components/ActiveRaffles";
 import HowItWorks from "@/components/HowItWorks";
-import LiveStream from "@/components/LiveStream";
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -10,7 +9,6 @@ const Index = () => {
       <Hero />
       <ActiveRaffles />
       <HowItWorks />
-      <LiveStream />
       <Footer />
     </div>
   );
