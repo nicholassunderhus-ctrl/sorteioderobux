931ade48e047864ede398935415f9c77b6f89b06
nicholasgloria <nicholassunderhus@gmail.com>
2025-11-29 23:06:14 -0300

    desbuga2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import GanharBilhetes from "./pages/GanharBilhetes";
 import { AuthProvider } from "./contexts/AuthContext";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/ganhar-bilhetes" element={<GanharBilhetes />} />
-      </Routes>
+      <div className="min-h-screen font-fredoka flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/ganhar-bilhetes" element={<GanharBilhetes />} />
+          </Routes>
+        </main>
+        {isHomePage && <Footer />}
+      </div>
     </AuthProvider>
   );
 }
