6b1af7b78a32d4c14b29fd6c528b36ae17f8d4e4
nicholasgloria <nicholassunderhus@gmail.com>
2025-12-01 20:50:37 -0300

    yasmim17

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -50,7 +50,7 @@ const HowItWorks = () => {
         </div>
 
         {/* Steps Grid */}
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-6xl mx-auto">
           {steps.map((step, index) => {
             const Icon = step.icon;
             return (
